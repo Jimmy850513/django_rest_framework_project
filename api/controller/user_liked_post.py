a2cce8196e9cc74b4aa1e5346e75ccf7eac09f18
appmaker <JiangJimmy@dosuper.com.tw>
2024-08-01 12:04:19 +0800

    complete

--- api/controller/user_liked_post.py
+++ api/controller/user_liked_post.py
@@ -5,13 +5,60 @@ from ..models.user_models import UserProfile_models
 from ..models.post_models import PostModels
 from ..models.post_models import PostModels_Comment
 from ..models.user_models import User_Follower
+from ..models.user_models import User_Follow_Post
 from datetime import datetime
 from rest_framework import status
 from django.forms.models import model_to_dict
 from ..utils import get_user_id_and_username
 
 class User_Liked_Post(APIView):
+    #選出該使用者喜歡的全部文章
     def get(self,request,*args,**kwargs):
-        return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN})
-   
-        
+        author_headers = request.headers.get('Authorization',None)
+        user_id,user_name = get_user_id_and_username(author_headers)
+        if user_id and user_name:
+            try:
+                like_post = User_Follow_Post.objects.filter(user_id=user_id,user_name=user_name,like_post=True).values()
+                for data in like_post:
+                    post_id = data['post_id']
+                    if PostModels.objects.filter(post_id=post_id).exists():
+                        like_post = [{**data}]
+                    else:
+                        pass
+                if like_post:
+                    return Response({"msg":f"{user_name}所喜歡的文章","data":like_post,"status":status.HTTP_200_OK})
+                else:
+                    return Response({"msg":"目前無喜歡的文章","status":status.HTTP_200_OK})
+            except Exception as e:
+                return Response({"msg":f"發生錯誤:{e}","status":status.HTTP_500_INTERNAL_SERVER_ERRORT})
+        else:
+            return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN})
+    def post(self,request,*args,**kwargs):
+        author_headers = request.headers.get('Authorization',None)
+        user_id,user_name = get_user_id_and_username(author_headers)
+        data_form = request.data
+        if user_id and user_name:
+            post_id = data_form.get("post_id")
+            if post_id:
+                try:
+                    post_id_data = PostModels.objects.get(post_id=post_id)
+                    post_id_dict = model_to_dict(post_id_data)
+                    if post_id_dict['user'] == user_id:
+                        return Response({"msg":"該文章是你自己的喔,相信你會喜歡你自己","status":status.HTTP_400_BAD_REQUEST})
+                except Exception as e:
+                    print(e)
+                    return Response({"msg":"找不到該篇文章,文章主人可能已經刪除了","status":status.HTTP_404_NOT_FOUND})
+                if post_id_dict:
+                    try:
+                        User_Follow_Post.objects.create(post_id=post_id_dict['post_id'],
+                                                        user_id=user_id,
+                                                        user_name=user_name,
+                                                        like_post=True
+                                                        )
+                        return Response({"msg":f"{user_name}喜歡文章{post_id}成功","status":status.HTTP_200_OK})
+                    except Exception as e:
+                        return Response({"msg":f"發生錯誤：{e}","status":status.HTTP_500_INTERNAL_SERVER_ERROR})                
+            else:
+                return Response({"msg":"請輸入你要追蹤的文章編號！","status":status.HTTP_400_BAD_REQUEST})
+        else:
+            return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN})
--- api/controller/user_liked_post_specifice.py
+++ api/controller/user_liked_post_specifice.py
@@ -5,13 +5,57 @@ from ..models.user_models import UserProfile_models
 from ..models.post_models import PostModels
 from ..models.post_models import PostModels_Comment
 from ..models.user_models import User_Follower
+from ..models.user_models import User_Follow_Post
 from datetime import datetime
 from rest_framework import status
 from django.forms.models import model_to_dict
 from ..utils import get_user_id_and_username
 
 class User_Liked_Post_Spec(APIView):
+    #顯示單篇喜歡的文章
     def get(self,request,*args,**kwargs):
-        return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN})
-   
-        
+        post_id = self.kwargs.get("post_id",None)
+        author_headers = request.headers.get("Authorization",None)
+        user_id,user_name = get_user_id_and_username(author_headers)
+        if user_id and user_name:
+            try:
+                post = PostModels.objects.get(post_id=post_id)
+                post_dict = model_to_dict(post)
+                post_liked_spec = User_Follow_Post.objects.filter(post_id=post_id,user_id=user_id,like_post=True).values()
+                post_yourself = PostModels.objects.filter(user_id=user_id,post_id=post_id).values()
+                if post_yourself:
+                    return Response({"msg":"該文章文自己的文章","status":status.HTTP_400_BAD_REQUEST})
+                else:
+                    if post_liked_spec:
+                        return Response({"msg":"找到該文章成功","data":post_dict,"status":status.HTTP_200_OK})
+                    else:
+                        return Response({"msg":"尚未喜歡該文章,要先去喜歡才可以查看","status":status.HTTP_400_BAD_REQUEST})
+            except Exception as e:
+                print(e)
+                return Response({"msg":"找不到該文章,可能已經被刪除了","status":status.HTTP_404_NOT_FOUND})  
+        else:
+            return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN})
+    #取消追蹤喜歡的文章
+    def patch(self,request,*args,**kwargs):
+        post_id = self.kwargs.get('post_id',None)
+        author_headers = request.headers.get("Authorization",None)
+        user_id,user_name = get_user_id_and_username(author_headers)
+        if user_id and user_name:
+            try:
+                post = PostModels.objects.get(post_id=post_id)
+                post_dict = model_to_dict(post)
+                post_yourself = PostModels.objects.filter(user_id=user_id,post_id=post_id).values()
+                if post_yourself:
+                    return Response({"msg":"該文章文自己的文章,不可以取消追蹤","status":status.HTTP_400_BAD_REQUEST})
+                
+                post_liked_spec = User_Follow_Post.objects.filter(post_id=post_id,user_id=user_id).exists()
+                if post_liked_spec:
+                    User_Follow_Post.objects.filter(post_id=post_id,user_id=user_id).update(like_post=False)
+                    return Response({"msg":"取消追蹤成功","status":status.HTTP_200_OK})
+                else:
+                    return Response({"msg":"尚未喜歡該文章,無法執行取消追蹤","status":status.HTTP_400_BAD_REQUEST})
+            except Exception as e:
+                print(e)
+                return Response({"msg":"找不到該文章,可能已經被刪除了","status":status.HTTP_404_NOT_FOUND})  
+        else:
+            return Response({"msg":"權限不足請先登入","status":status.HTTP_403_FORBIDDEN}) 
